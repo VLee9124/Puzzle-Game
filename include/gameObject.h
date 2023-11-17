7b507a4d46af3f962fd347e82b1ec4b1d12b2131
Victor Lee <victorleefca@gmail.com>
2023-11-17 12:15:02 -0800

    Created object header files

--- include/gameObject.h
+++ include/gameObject.h
@@ -3,43 +3,36 @@
 #include <string>
 #include <vector>
 
-/*General abstract object that can be interacted with. This item cannot be collected.*/
+using namespace std;
+
 class gameObject
 {
 private:
-  std::string name;
-  std::string description;
-  std::string interaction;
+  string name;
+  string description;
+  string interaction;
 
 public:
-  gameObject(std::string name, std::string description, std::string interaction);
+  gameObject(string name, string description, string interaction);
 
-  // Getters and setters
-  std::string getName() { return name; }
-  std::string getDescription() { return description; }
-  std::string getInteraction() { return interaction; }
-  void setName(std::string s) { name = s; }
-  void setDescription(std::string s) { description = s; }
-  void setInteraction(std::string s) { interaction = s; }
+  string getName();
+  string getDesc();
+  string getInteraction();
 
-  // Game object functions
-  virtual void interact() = 0;
+  void setName();
+  void setDesc();
+  void setInteraction();
+
+  void interact();
 };
 
-/*An interactable object that can be collected and stored in the inventory.*/
-class inventoryObject : public gameObject
+class InventoryObject : public gameObject
 {
 private:
-  bool collected = false;
-  std::string collectionMessage;
+  string collectionMessage;
 
 public:
-  inventoryObject(std::string name, std::string description, std::string interaction);
-
-  // Getters and setters
-  std::string getCollectionMessage() { return collectionMessage; }
-  void setCollectionMessage(std::string s) { collectionMessage = s; }
-
-  // Inventory object functions
-  virtual void collect() = 0;
+  InventoryObject(string name, string description, string interaction, string collectionMessage);
+  void interact() override;
+  void collect();
 };
