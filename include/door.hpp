8254f046b1184ea35868ca07c933f3d066cef34f
chris-hakker <christopherhakker@gmail.com>
2023-11-21 20:18:24 -0800

    Add door class

---
+++ include/door.hpp
@@ -0,0 +1,20 @@
+#ifndef DOOR_HPP
+#define DOOR_HPP
+
+#include <string>
+#include "gameObject.hpp"
+#include "room.hpp"
+
+class Door : public gameObject {
+    private:
+        Room* adjacentRoom;
+        bool isOpen;
+    public:
+        Door(Room* adjRoom);
+        Room* getAdjacentRoom();
+        void setOpen(bool val);
+        bool getOpen();
+        void interact() override;
+};
+
+#endif /* DOOR_HPP */
